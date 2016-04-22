25e45854cfb7e22d1fc3ceb81659e44e4559775c
Tom Williamson <tom@dtssoftware.com>
2016-04-22 11:38:11 -0400

    Initial Commit

---
+++ Hero2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hero2
+//
+//  Created by Tom Williamson on 4/22/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hero2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hero2
+//
+//  Created by Tom Williamson on 4/22/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
